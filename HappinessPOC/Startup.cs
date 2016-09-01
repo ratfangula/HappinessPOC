50de0966198556786e164802b02ac67bec89a587
ratim <ratim@DESKTOP-QQUSUV3>
2016-09-01 13:46:01 -0400

    Add project files.

---
+++ HappinessPOC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HappinessPOC.Startup))]
+namespace HappinessPOC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
